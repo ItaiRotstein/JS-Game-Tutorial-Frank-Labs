2c9b56eaf5f262293bf3b38ec2b11b7a32a500e4
Itai Rotstein <itairotstein@gmail.com>
2023-12-25 00:03:44 +0300

    added jump

--- player.js
+++ player.js
@@ -6,6 +6,7 @@ export class Player {
         this.x = 0
         this.y = this.game.height - this.height
         this.vy = 0 //vy means Velocity Y
+        this.weight = 1
         this.image = document.getElementById('player')
         this.speed = 0
         this.maxSpeed = 10
@@ -18,8 +19,16 @@ export class Player {
         else this.speed = 0
         if (this.x < 0) this.x = 0
         if (this.x + this.width > this.game.width) this.x = this.game.width - this.width
+        //vertical movement
+        if (input.includes('ArrowUp') && this.onGround()) this.vy -=30
+        this.y += this.vy
+        if (!this.onGround()) this.vy += this.weight
+        else this.vy = 0
     }
     draw(context) {
         context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height)
+    }
+    onGround() {
+        return this.y >= this.game.height - this.height        
     }
 }
